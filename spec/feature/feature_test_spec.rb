d2e72ac7295e187468a16f616611532edbc05347
Léa R <lea.rosamond@gmail.com>
2017-12-11 17:15:20 +0000

    Step 9 - creating link.rb, creating database and connecting it to link

--- spec/feature/feature_test_spec.rb
+++ spec/feature/feature_test_spec.rb
@@ -1,7 +1,7 @@
-feature "homepage" do 
+feature "homepage" do
 
   scenario "shows list of bookmarks" do
-  	Link.create(Url: "https://github.com", name: :Github)
+  	Link.create(url: "https://github.com", title: :Github)
   	visit '/'
   	expect(page).to have_content("Bookmarks:") and have_link("Github")
   end
